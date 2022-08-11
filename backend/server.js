367f2b5c33738a6f3a2e4f709cea3935eeeb6acf
Christian Chang <christianchangthomaz@gmail.com>
2022-08-11 18:27:56 -0300

    Create Sample Products in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
@@ -10,20 +10,8 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useUnifiedTopology: true,
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not Found" });
-  }
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
@@ -36,5 +24,5 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
+  console.log(`Server at http://localhost:${port}`);
 });
